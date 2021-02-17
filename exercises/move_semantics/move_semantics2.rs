5b773505a141a896a2130014ed7131782e3d92c7
Mikhail <mmvsha73@gmail.com>
2021-02-17 23:35:40 +0400

    move semantics 2

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -5,24 +5,75 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    fill_vec(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
-    vec1.push(88);
+    vec0.push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec1", vec0.len(), vec0);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(vec: &mut Vec<i32>) {
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
-    vec
 }
+
+// Had to use hints there
+
+// 1. Make another, separate version of the data that's in `vec0` and pass that
+// to `fill_vec` instead.
+
+// fn main() {
+//     let vec0 = Vec::new();
+//     let mut vec1 = fill_vec(vec0.clone());
+//     // Do not change the following line!
+//     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+//     vec1.push(88);
+//     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+// }
+
+// 2. Make `fill_vec` borrow its argument instead of taking ownership of it,
+// and then copy the data within the function in order to return an owned
+// `Vec<i32>`
+
+// fn main() {
+//     let vec0 = Vec::new();
+//     let mut vec1 = fill_vec(&vec0);
+//     // Do not change the following line!
+//     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+//     vec1.push(88);
+//     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+// }
+
+// fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+//     let mut vec = vec.clone();
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+//     vec
+// }
+
+// 3. Make `fill_vec` *mutably* borrow its argument (which will need to be
+// mutable), modify it directly, then not return anything. Then you can get rid
+// of `vec1` entirely -- note that this will change what gets printed by the
+// first `println!`
+
+// fn main() {
+//     let mut vec0 = Vec::new();
+//     fill_vec(&mut vec0);
+//     // Do not change the following line!
+//     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+//     vec0.push(88);
+//     println!("{} has length {} content `{:?}`", "vec1", vec0.len(), vec0);
+// }
+
+// fn fill_vec(vec: &mut Vec<i32>) {
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+// }
